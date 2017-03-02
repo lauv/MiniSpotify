ce4866d2b273e450ddff89b46d29ee71e71e486e
Laura Díaz Vera <valentina.hp@gmail.com>
2017-03-02 10:54:02 -0300

    Segundo commit: creados los modelos y controladores

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :songs
+  resources :genres
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
